d34837a4d215dd6a9ea2faa723d046e752febc8a
Rebeca Robbo <rebeca.lqa@gmail.com>
2022-11-07 14:55:53 -0300

    Customizando exceptions

---
+++ src/main/java/com/robbo/calculator/execeptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.robbo.calculator.execeptions.handler;
+
+import com.robbo.calculator.execeptions.ExceptionResponse;
+import org.apache.catalina.WebResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice /*Para cair no tratamento de excessao global*/
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsupportedOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadExceptions(Exception ex, WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+}
